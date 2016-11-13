a428564eaabe3966bc44b31351ecc5b13e04cc9a
BowersEr-372 <Bowerser@Oregonstate.edu>
2016-11-13 14:29:57 -0800

    Update nodetest.js

--- nodetest.js
+++ nodetest.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(req,res){
   res.writeHead(200, { 
-  Content-Type': 'text/plain' });
+  'Content-Type': 'text/plain' });
   res.end('Is this working?');
 }).listen(8438);
 
