af68cc2b9f91eb35d08df038b5514f034e039fb6
BowersEr-372 <Bowerser@Oregonstate.edu>
2016-11-13 14:20:08 -0800

    Create nodetest.js

---
+++ nodetest.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req,res){
+  res.writeHead(200, { 
+  Content-Type': 'text/plain' });
+  res.end('Is this working?');
+}).listen(8438);
+
+console.log('Server started on localhost:stuff; press Ctrl-C to terminate....');
